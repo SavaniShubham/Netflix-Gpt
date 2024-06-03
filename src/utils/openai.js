740c9ece8345608a50d94de76434cdfc9ef30405
SavaniShubham <shubham.savani2004@gmail.com>
2024-06-03 11:07:28 +0530

    install gemini/openai, get api- key , do  congiuration ,write function for handlesearchclick

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -1,11 +1,11 @@
 
-import OpenAI from 'openai';
-import {OPENAI_KEY} from './constant';
+// import OpenAI from 'openai';
+// import {OPENAI_KEY} from './constant';
 
-const openai = new OpenAI({
-  apiKey: OPENAI_KEY , // defaults to process.env["OPENAI_API_KEY"]
-  dangerouslyAllowBrowser: true ,
-});
+// const openai = new OpenAI({
+//   apiKey: OPENAI_KEY , // defaults to process.env["OPENAI_API_KEY"]
+//   dangerouslyAllowBrowser: true ,
+// });
 
 
-export default openai;
+// export default openai;
