849661bd43288a49a0bc3473a8f6dffd01872f30
SavaniShubham <shubham.savani2004@gmail.com>
2024-06-04 16:39:27 +0530

    deploy  the app in firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDTLAWPaf4qNK5sXsfmtFlkX_kLNNogGLI",
-  authDomain: "netflixgpt-9ce3a.firebaseapp.com",
-  projectId: "netflixgpt-9ce3a",
-  storageBucket: "netflixgpt-9ce3a.appspot.com",
-  messagingSenderId: "1051122746089",
-  appId: "1:1051122746089:web:49d9affc5cd0a42e106678",
-  measurementId: "G-QQYM081N86"
+  apiKey: "AIzaSyDd-FJfk2F4LHVnUKPZ1YsZRm1icu9Bx-Q",
+  authDomain: "neflixgemini.firebaseapp.com",
+  projectId: "neflixgemini",
+  storageBucket: "neflixgemini.appspot.com",
+  messagingSenderId: "80780776729",
+  appId: "1:80780776729:web:1604454073aef611b12188",
+  measurementId: "G-5XQ2M3HMGT"
 };
 
 // Initialize Firebase
